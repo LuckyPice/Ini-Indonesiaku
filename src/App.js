32ecc3c1b042378ca26103571d8c44d8f30162dd
salmalatifa <salma.lathifah17@gmail.com>
2022-06-07 20:17:42 +0700

    detail destination react
    coba menggunakan react, belum responsive, belum bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
 
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-
-import "./assets/scss/style.scss"
+import "./assets/scss/style.scss";
 import PageHotel from "./pages/PageHotel";
+import PageDestination from "../"
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Route path="/" component={PageHotel}></Route>
-      </Router>
-    </div>
-  );
+    return ( <div className = "App" >
+        <Router >
+        <Route path = "/" component = { PageHotel } ></Route>
+        <Route path = "/" component = {PageDestination}></Route>
+        </Router > </div>
+    );
 }
 
 export default App;
--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -6,7 +6,7 @@ import IconTraveler from "../assets/images/icons/ic_traveler.svg";
 import IconCities from "../assets/images/icons/ic_cities.svg";
 import IconTreasure from "../assets/images/icons/ic_treasure.svg";
 
-import Button from "../elements/Button";
+import Button from "../elements/button";
 
 import formatNumber from "utils/formatNumber";
 
@@ -25,24 +25,24 @@ export default function Hero(props) {
           <div className="col-auto pr-5" style={{ width: 530 }}>
             <h1 className="font-weight-bold line-height-1 mb-3">
               Enjoy your Holiday <br />
-              With Best Staycasion
-            </h1>
+              With Best Staycasion{" "}
+            </h1>{" "}
             <p
               className="mb-4 font-weight-light text-gray-500 w-75"
-              style={{lineHeight:"170%"}}
+              style={{ lineHeight: "170%" }}
             >
-              Kami merekomendasikan staycasion terbaik untuk anda untuk membuat 
-              pengalaman liburan anda yang indah bersama keluarga dan orang terdekat
-            </p>
+              Kami merekomendasikan staycasion terbaik untuk anda untuk membuat
+              pengalaman liburan anda yang indah bersama keluarga dan orang
+              terdekat{" "}
+            </p>{" "}
             <Button
               className="btn px-5"
               hasShadow
               isPrimary
               onClick={showMostPicked}
             >
-              Show Me Now
+              Show Me Now{" "}
             </Button>
-
             <div className="row" style={{ marginTop: 80 }}>
               <div className="col-auto" style={{ marginRight: 35 }}>
                 <img
@@ -50,45 +50,47 @@ export default function Hero(props) {
                   height="36"
                   src={IconTraveler}
                   alt={`${props.data.travelers} Travelers`}
-                />
+                />{" "}
                 <h6 className="mt-3">
+                  {" "}
                   {formatNumber(props.data.travelers)}{" "}
                   <span className="text-gray-500 font-weight-light">
-                    travelers
-                  </span>
-                </h6>
-              </div>
+                    travelers{" "}
+                  </span>{" "}
+                </h6>{" "}
+              </div>{" "}
               <div className="col-auto" style={{ marginRight: 35 }}>
                 <img
                   width="36"
                   height="36"
                   src={IconTreasure}
                   alt={`${props.data.treasures} Treasures`}
-                />
+                />{" "}
                 <h6 className="mt-3">
+                  {" "}
                   {formatNumber(props.data.treasures)}{" "}
                   <span className="text-gray-500 font-weight-light">
-                    treasures
-                  </span>
-                </h6>
-              </div>
+                    treasures{" "}
+                  </span>{" "}
+                </h6>{" "}
+              </div>{" "}
               <div className="col-auto">
                 <img
                   width="36"
                   height="36"
                   src={IconCities}
                   alt={`${props.data.cities} Cities`}
-                />
+                />{" "}
                 <h6 className="mt-3">
+                  {" "}
                   {formatNumber(props.data.cities)}{" "}
                   <span className="text-gray-500 font-weight-light">
-                    cities
-                  </span>
-                </h6>
-              </div>
-            </div>
+                    cities{" "}
+                  </span>{" "}
+                </h6>{" "}
+              </div>{" "}
+            </div>{" "}
           </div>
-
           <div className="col-6 pl-5">
             <div style={{ width: 520, height: 410 }}>
               <img
@@ -96,17 +98,17 @@ export default function Hero(props) {
                 alt="Room with couches"
                 className="img-fluid position-absolute"
                 style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
-              />
+              />{" "}
               <img
                 src={ImageHero_}
                 alt="Room with couches frame"
                 className="img-fluid position-absolute"
                 style={{ margin: "0 -15px -15px 0" }}
-              />
-            </div>
-          </div>
-        </div>
-      </section>
-      </Fade>
+              />{" "}
+            </div>{" "}
+          </div>{" "}
+        </div>{" "}
+      </section>{" "}
+    </Fade>
   );
 }
