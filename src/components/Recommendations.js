1c8b28b58ae70c7f50dd55a7c9101474c70eefa3
Anrian Sihotang <anrishotang91@gmail.com>
2022-06-07 17:36:35 +0700

    not responsive

--- src/components/Recommendations.js
+++ src/components/Recommendations.js
@@ -1,15 +1,22 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, { Component } from "react";
-import { Col,Button } from "react-bootstrap";
+import { Col,Button,Card,Row } from "react-bootstrap";
 import "../assets/style/Recommendations.css";
 import bali from "../assets/image/content/balli-reccomend.jpg";
+import ukm1 from "../assets/image/content/UKM-1.jpg";
+import ukm2 from "../assets/image/content/UKM-2.jpeg";
+import ukm3 from "../assets/image/content/UKM-3.jpg";
+import ukm4 from "../assets/image/content/UKM-4.jpg";
+
 class Recommendations extends Component {
 	render() {
 		return (
-			<Col xl={12} className="reccomend-card">
-				<div>
-					<img src={bali} className="reccomend-img"/>
-				</div>
+			<div>
+				<Row>
+			<Col className="reccomend-card">
+				<img src={bali} className="reccomend-img"/>
+			</Col>
+			<Col className="reccomend-card">
 				<div className="reccomend-text">
 					<h5>Rekomendasi Destinasi</h5>
 					<h3>Wonderfull Bali</h3>
@@ -21,7 +28,61 @@ class Recommendations extends Component {
 					</p>
 					<Button variant="success">Read More</Button>
 				</div>
-			</Col>	
+			</Col>
+			</Row>
+			<Col>
+				<h3>Rekomendasi UMKM</h3>
+				<p>Temukan pernak penik pilihan yang dibuat langsung oleh warga sekitar destinasi</p>
+				<Row >
+					<Card style={{ width: '18rem' }} className="cardukm">
+						<Card.Img variant="top" src={ukm1} className="imgukm"/>
+						<Card.Body>
+							<Card.Text>20-11-2022</Card.Text>
+							<Card.Title>UMKM Bali yang wajib didatangi!</Card.Title>
+						</Card.Body>
+						<Button variant="success">Read More</Button>
+					</Card>
+					<Card style={{ width: '18rem' }} className="cardukm">
+						<Card.Img variant="top" src={ukm2} className="imgukm" />
+						<Card.Body>
+							<Card.Text>20-11-2022</Card.Text>
+							<Card.Title>Cari oleh-oleh ? Cek 5 UMKM dari Bandung ini!</Card.Title>
+						</Card.Body>
+						<Button variant="success">Read More</Button>
+					</Card>
+					<Card style={{ width: '18rem' }} className="cardukm">
+						<Card.Img variant="top" src={ukm3} className="imgukm"/>
+						<Card.Body>
+							<Card.Text>20-11-2022</Card.Text>
+							<Card.Title>Rekomendasi UMKM legenda yang ada di Surakarta</Card.Title>
+						</Card.Body>
+						<Button variant="success">Read More</Button>
+					</Card>
+					<Card style={{ width: '18rem' }} className="cardukm">
+						<Card.Img variant="top" src={ukm4} className="imgukm"/>
+						<Card.Body>
+							<Card.Text>20-11-2022</Card.Text>
+							<Card.Title>Simak UMKM unik dari tanah NTT</Card.Title>
+						</Card.Body>
+						<Button variant="success">Read More</Button>
+					</Card>
+				</Row>
+				<Row className="rating">
+					<Col >
+						<img src="https://img.alinea.id/img/content/2019/01/05/24169/film-keluarga-cemara-nostalgia-drama-keluarga-yang-segar-2l2GpenZgh.jpg" />
+					</Col>
+					<Col>
+						<h2>Apa kata mereka ?</h2>
+						<h6>⭐⭐⭐⭐⭐</h6>
+						<p className="ratingtext">Perjalanan liburan yang sangat menyenangkan
+							tidak sabar untuk liburan selanjutnya
+						</p>
+						<p className="ratingnama">Budi, Product Manager</p>
+						<Button variant="success">Read Their Story</Button>
+					</Col>
+				</Row>
+			</Col>
+			</div>
 		)
 	}
 }
